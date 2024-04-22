c311b36cb14c893367062a620b12fbc4be7ad564
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-04-22 20:48:31 +0900

    Fix_CharacterClass
    public 으로 모두 전환

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -6,50 +6,56 @@
 
         static void Main(string[] args)
         {
+            bool gameOn = true;
+
             //MakeCharacter();
-
             Character character = new Character();
-            character.Level = 1;
-            character.Name = "Jack";
-            character.Job = "전사";
-            character.AttackValue = 10;
-            character.DefenseValue = 5;
-            character.HitPoint = 100;
-            character.Gold = 1500;
+            character.level = 1;
+            character.name = "Jack";
+            character.job = "전사";
+            character.attackValue = 10;
+            character.defenseValue = 5;
+            character.hitPoint = 100;
+            character.gold = 1500;
 
-            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.\n이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
-            Console.WriteLine("\n1. 상태 보기\n2. 인벤토리\n3. 상점");
-            Console.WriteLine("\n원하시는 행동을 입력해주세요.");
-            Console.Write(">>");
-
-            int firstAction = GameStart();
-            
-            switch(firstAction)
+            do
             {
-                case 1:                                 // 1. 상태 보기
-                    Console.Clear();
-                    Console.WriteLine("상태 보기");
-                    Console.WriteLine("캐릭터의 정보가 표시됩니다.\n");
-                    Console.WriteLine("Lv. {0:D2}", character.Level);
-                    Console.WriteLine($"{character.Name} ( {character.Job} )");
-                    Console.WriteLine($"공격력 : {character.AttackValue}");
-                    Console.WriteLine($"방어력 : {character.DefenseValue}");
-                    Console.WriteLine($"체 력 : {character.HitPoint}");
-                    Console.WriteLine($"Gold : {character.Gold} G");
-                    Console.WriteLine("\n0. 나가기\n\n원하시는 행동을 입력해주세요.");
-                    Console.Write(">>");
+                Console.Clear();
+                Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.\n이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
+                Console.WriteLine("\n1. 상태 보기\n2. 인벤토리\n3. 상점");
+                Console.WriteLine("\n원하시는 행동을 입력해주세요.");
+                Console.Write(">>");
 
-                    int statusAction = int.Parse(Console.ReadLine());
+                int firstAction = GameStart();
 
+                switch (firstAction)
+                {
+                    case 1:                                 // 1. 상태 보기
+                        Console.Clear();
+                        Console.WriteLine("상태 보기");
+                        Console.WriteLine("캐릭터의 정보가 표시됩니다.\n");
+                        Console.WriteLine("Lv. {0:D2}", character.level);
+                        Console.WriteLine($"{character.name} ( {character.job} )");
+                        Console.WriteLine($"공격력 : {character.attackValue}");
+                        Console.WriteLine($"방어력 : {character.defenseValue}");
+                        Console.WriteLine($"체 력 : {character.hitPoint}");
+                        Console.WriteLine($"Gold : {character.gold} G");
+                        Console.WriteLine("\n0. 나가기\n\n원하시는 행동을 입력해주세요.");
+                        Console.Write(">>");
 
-                    break;
-                case 2:
-                    Console.WriteLine("2. 인벤토리를 골랐습니다.");
-                    break;
-                case 3:
-                    Console.WriteLine("3. 상점을 골랐습니다.");
-                    break;
+                        int statusAction = SelectStatusAction();  // 상태창 액션 선택
+
+                        break;
+                    case 2:
+                        Console.WriteLine("2. 인벤토리를 골랐습니다.");
+                        break;
+                    case 3:
+                        Console.WriteLine("3. 상점을 골랐습니다.");
+                        gameOn = false;
+                        break;
+                }
             }
+            while (gameOn);
         }
 
         static public int GameStart()
@@ -75,6 +81,38 @@
             while (!isNumber);
 
             if (isNumber) 
+            {
+                return select;
+            }
+            else
+            {
+                return select;
+            }
+        }
+
+        static public int SelectStatusAction()
+        {
+            int select = 0;
+            bool isNumber;
+            do
+            {
+                string input = Console.ReadLine();
+                isNumber = int.TryParse(input, out select);
+                if (!isNumber)
+                {
+                    Console.WriteLine("잘못된 입력입니다.");
+                    Console.Write(">>");
+                }
+                else if (select < 0 || select > 0)
+                {
+                    Console.WriteLine("잘못된 입력입니다.");
+                    Console.Write(">>");
+                    isNumber = false;
+                }
+            }
+            while (!isNumber);
+
+            if (isNumber)
             {
                 return select;
             }
@@ -132,89 +170,13 @@
 
     public class Character
     {
-        int level = 0;
-        string name = "";
-        string job = "";
-        int attackValue = 0;
-        int defenseValue = 0;
-        int hitPoint = 0;
-        int gold = 0;
+        public int level = 0;
+        public string name = "";
+        public string job = "";
+        public int attackValue = 0;
+        public int defenseValue = 0;
+        public int hitPoint = 0;
+        public int gold = 0;
 
-        //public int ViewLevel()
-        //{
-        //    return level;
-        //}
-
-        //public string ViewName()
-        //{
-        //    return name;
-        //}
-
-        //public string ViewJob()
-        //{
-        //    return job;
-        //}
-
-        //public int ViewAttackValue()
-        //{
-        //    return attackValue;
-        //}
-
-        //public int ViewDefenseValue()
-        //{
-        //    return defenseValue;
-        //}
-
-        //public int ViewHitPoint()
-        //{
-        //    return hitPoint;
-        //}
-
-        //public int ViewGold()
-        //{
-        //    return gold;
-        //}
-
-        public int Level
-        {
-            get { return level; } 
-            set {  level = value; }
-        }
-
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        public string Job
-        {
-            get { return job; }
-            set { job = value; }
-        }
-
-        public int AttackValue
-        {
-            get { return attackValue; }
-            set { attackValue = value; }
-        }
-
-        public int DefenseValue
-        {
-            get { return defenseValue; }
-            set { defenseValue = value; }
-        }
-
-        public int HitPoint
-        {
-            get { return hitPoint; }
-            set { hitPoint = value; }
-        }
-
-        public int Gold
-        {
-            get { return gold; }
-            set { gold = value; }
-        }
     }
 }
--- Game/Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
+++ Game/Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b183b61ee2f156d19b6755cb1780224db43953ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebe0f8a974f48390f65e380ba5dc0c6fc212a0fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
