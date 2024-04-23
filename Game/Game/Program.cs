6b3ad8616520df5ea2c5347dd170a4a4b275d786
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-04-23 12:21:58 +0900

    Feature_SellItem
    아이템 판매 구현

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -14,23 +14,26 @@ namespace Game
             bool quitEquipment = false;
             bool shopQuitCheck = false;             
             bool buyShopItemCheck = false;             // 상점 확인
+            bool quitSellMyItemCheck = false;
             
 
             // 아이템 클래스 생성
             Item noviceArmor = new Item();
-            noviceArmor.SetItem("수련자 갑옷     ", 2, 5  , "수련에 도움을 주는 갑옷입니다.                   ", 1000);
+            noviceArmor.SetItem("수련자 갑옷     ", 2, 5  , "수련에 도움을 주는 갑옷입니다.                   ", 1000, "수련자 갑옷");
             Item ironArmor = new Item();
-            ironArmor.SetItem("무쇠갑옷        ", 2, 9  , "무쇠로 만들어져 튼튼한 갑옷입니다.               ", 1800);
+            ironArmor.SetItem("무쇠갑옷        ", 2, 9  , "무쇠로 만들어져 튼튼한 갑옷입니다.               ", 1800, "무쇠갑옷");
             Item spartanArmor = new Item();
-            spartanArmor.SetItem("스파르타의 갑옷 ", 2, 15 , "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 3500);
+            spartanArmor.SetItem("스파르타의 갑옷 ", 2, 15 , "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 3500, "스파르타의 갑옷");
             Item wornSword = new Item();
-            wornSword.SetItem("낡은 검         ", 1, 2  , "쉽게 볼 수 있는 낡은 검 입니다.                  ", 600);
+            wornSword.SetItem("낡은 검         ", 1, 2  , "쉽게 볼 수 있는 낡은 검 입니다.                  ", 600, "낡은 검");
             Item bronzeAxe = new Item();
-            bronzeAxe.SetItem("청동 도끼       ", 1, 5  , "어디선가 사용됐던거 같은 도끼입니다.             ", 1500);
+            bronzeAxe.SetItem("청동 도끼       ", 1, 5  , "어디선가 사용됐던거 같은 도끼입니다.             ", 1500, "청동 도끼");
             Item spartanSpear = new Item();
-            spartanSpear.SetItem("스파르타의 창   ", 1, 7  , "스파르타의 전사들이 사용했다는 전설의 창입니다.  ", 2700);
+            spartanSpear.SetItem("스파르타의 창   ", 1, 7  , "스파르타의 전사들이 사용했다는 전설의 창입니다.  ", 2700, "스파르타의 창");
+            Item legendarySword = new Item();
+            legendarySword.SetItem("전설의 검       ", 1, 80, "용사를 위한 전설의 검 입니다.                    ", 100, "전설의 검");
 
-            Item[] shopItems = new Item[] { noviceArmor, ironArmor, spartanArmor, wornSword, bronzeAxe, spartanSpear };
+            Item[] shopItems = new Item[] { noviceArmor, ironArmor, spartanArmor, wornSword, bronzeAxe, spartanSpear, legendarySword };
 
             List<Item> myItems = new List<Item>();
 
@@ -63,20 +66,7 @@ namespace Game
                         gameOn = false;
                         break;
                     case 1:                                 // 1. 상태 보기
-                        Console.Clear();
-                        Console.WriteLine("상태 보기");
-                        Console.WriteLine("캐릭터의 정보가 표시됩니다.\n");
-                        Console.WriteLine("Lv. {0:D2}", character.level);
-                        Console.WriteLine($"{character.name} ( {character.job} )");
-                        Console.WriteLine($"공격력 : {character.attackValue}");
-                        Console.WriteLine($"방어력 : {character.defenseValue}");
-                        Console.WriteLine($"체 력 : {character.hitPoint}");
-                        Console.WriteLine($"Gold : {character.gold} G");
-                        Console.WriteLine("\n0. 나가기\n\n원하시는 행동을 입력해주세요.");
-                        Console.Write(">>");
-
-                        int statusAction = SelectStatusAction();  // 상태창 액션 선택
-
+                        ViewStatus();
                         break;
                     case 2:
                         quitInventory = false;
@@ -85,7 +75,6 @@ namespace Game
                             ViewInventory();
                         }
                         while (!quitInventory);
-
 
                         break;
                     case 3:
@@ -96,14 +85,70 @@ namespace Game
                         }
                         while (!shopQuitCheck);
                         
-
                         break;
                 }
             }
             while (gameOn);
 
 
+            void ViewStatus()
+            {
+                int attackItemValue = 0;
+                int defenseItemValue = 0;
 
+                Console.Clear();
+                Console.WriteLine("상태 보기");
+                Console.WriteLine("캐릭터의 정보가 표시됩니다.\n");
+                Console.WriteLine("Lv. {0:D2}", character.level);
+                Console.WriteLine($"{character.name} ( {character.job} )");
+                foreach (Item item in myItems)                                  //무기 꼈는지 체크
+                {
+                    if (item.type == 1)            
+                    {
+                        if (item.equip)            
+                        {
+                            attackItemValue += item.value;
+                        }
+                    }
+                }
+
+                foreach (Item item in myItems)                      // 방어구 아이템 꼈는지 체크
+                {
+                    if (item.type == 2)             
+                    {
+                        if (item.equip)           
+                        {
+                            defenseItemValue += item.value;
+                        }
+                    }
+                }
+
+                if (attackItemValue == 0)
+                { 
+                    Console.WriteLine($"공격력 : {character.attackValue}");
+                }
+                else
+                {
+                    Console.WriteLine($"공격력 : {(character.attackValue + attackItemValue)} (+{attackItemValue})");
+                }
+
+                if (defenseItemValue == 0)
+                {
+                    Console.WriteLine($"방어력 : {character.defenseValue}");
+                }
+                else
+                {
+                    Console.WriteLine($"방어력 : {(character.defenseValue + defenseItemValue)} (+{defenseItemValue})");
+                }
+
+
+                Console.WriteLine($"체 력 : {character.hitPoint}");
+                Console.WriteLine($"Gold : {character.gold} G");
+                Console.WriteLine("\n0. 나가기\n\n원하시는 행동을 입력해주세요.");
+                Console.Write(">>");
+
+                int statusAction = SelectStatusAction();  // 상태창 액션 선택
+            }
 
             void ViewInventory()
             {
@@ -116,7 +161,15 @@ namespace Game
                     {
                         if (name.type == 1)                 // 무기
                         {
-                            Console.WriteLine($"- [E]{name.name}| 공격력 +{name.value}  | {name.information}");
+                            if (name.value > 9)
+                            {
+                                Console.WriteLine($"- [E]{name.name}| 공격력 +{name.value} | {name.information}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"- [E]{name.name}| 공격력 +{name.value}  | {name.information}");
+                            }
+
                         }
 
                         else if (name.type == 2)                //방어구
@@ -135,7 +188,14 @@ namespace Game
                     {
                         if (name.type == 1)                 // 무기
                         {
-                            Console.WriteLine($"- {name.name}   | 공격력 +{name.value}  | {name.information}");
+                            if (name.value > 9)
+                            {
+                                Console.WriteLine($"- {name.name}   | 공격력 +{name.value} | {name.information}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"- {name.name}   | 공격력 +{name.value}  | {name.information}");
+                            }
                         }
 
                         else if (name.type == 2)                //방어구
@@ -189,7 +249,14 @@ namespace Game
                     {
                         if (name.type == 1)                 // 무기
                         {
-                            Console.WriteLine($"- {i} [E]{name.name}| 공격력 +{name.value}  | {name.information}");
+                            if (name.value > 9)
+                            {
+                                Console.WriteLine($"- {i} [E]{name.name}| 공격력 +{name.value} | {name.information}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"- {i} [E]{name.name}| 공격력 +{name.value}  | {name.information}");
+                            }
                         }
 
                         else if (name.type == 2)                //방어구
@@ -208,7 +275,14 @@ namespace Game
                     {
                         if (name.type == 1)                 // 무기
                         {
-                            Console.WriteLine($"- {i} {name.name}   | 공격력 +{name.value}  | {name.information}");
+                            if (name.value > 9)
+                            {
+                                Console.WriteLine($"- {i} {name.name}   | 공격력 +{name.value} | {name.information}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"- {i} {name.name}   | 공격력 +{name.value}  | {name.information}");
+                            }
                         }
 
                         else if (name.type == 2)                //방어구
@@ -239,7 +313,18 @@ namespace Game
                 }
                 else
                 {
+                    foreach (Item item in myItems)      // 장착되어있는 장비 중 타입이 장착하려는 장비를 찾아서 장착 해제
+                    {
+                        if (item.equip)
+                        {
+                            if (item.type == myItems[equipAction - 1].type)
+                            {
+                                item.equip = false;
+                            }
+                        }
+                    }
                     myItems[equipAction - 1].equip = true;
+
                 }
             }
 
@@ -256,7 +341,14 @@ namespace Game
                     {
                         if (shopItems[i].type == 1)                 // 무기
                         {
-                            Console.WriteLine($"- {shopItems[i].name}| 공격력 +{shopItems[i].value}  | {shopItems[i].information}| {shopItems[i].price} G");
+                            if (shopItems[i].value > 9)
+                            {
+                                Console.WriteLine($"- {shopItems[i].name}| 공격력 +{shopItems[i].value} | {shopItems[i].information}| {shopItems[i].price} G");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"- {shopItems[i].name}| 공격력 +{shopItems[i].value}  | {shopItems[i].information}| {shopItems[i].price} G");
+                            }
                         }
 
                         else if (shopItems[i].type == 2)                //방어구
@@ -275,7 +367,14 @@ namespace Game
                     {
                         if (shopItems[i].type == 1)                 // 무기
                         {
-                            Console.WriteLine($"- {shopItems[i].name}| 공격력 +{shopItems[i].value}  | {shopItems[i].information}| 구매완료");
+                            if (shopItems[i].value > 9)
+                            {
+                                Console.WriteLine($"- {shopItems[i].name}| 공격력 +{shopItems[i].value} | {shopItems[i].information}| 구매완료");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"- {shopItems[i].name}| 공격력 +{shopItems[i].value}  | {shopItems[i].information}| 구매완료");
+                            }
                         }
 
                         else if (shopItems[i].type == 2)                //방어구
@@ -292,10 +391,10 @@ namespace Game
                     }
                 }
                 // 상점 리스트
-                Console.WriteLine("\n1. 아이템 구매\n0. 나가기\n\n원하시는 행동을 입력해주세요.");
+                Console.WriteLine("\n1. 아이템 구매\n2. 아이템 판매\n0. 나가기\n\n원하시는 행동을 입력해주세요.");
                 Console.Write(">>");
 
-                int shopAction = SelectInventoryAction();  // 상점 액션 선택
+                int shopAction = SelectNumber(2);  // 상점 액션 선택
 
                 switch (shopAction)
                 {
@@ -308,10 +407,17 @@ namespace Game
                         do
                         {
                             BuyShopItem();
-
                         }
                         while (!buyShopItemCheck);
 
+                        break;
+                    case 2:
+                        quitSellMyItemCheck = false;
+                        do
+                        {
+                            SellMyItem();
+                        }
+                        while (!quitSellMyItemCheck);
                         break;
                 }
             }
@@ -329,7 +435,14 @@ namespace Game
                     {
                         if (shopItems[i].type == 1)                 // 무기
                         {
-                            Console.WriteLine($"- {i + 1} {shopItems[i].name}| 공격력 +{shopItems[i].value}  | {shopItems[i].information}| {shopItems[i].price} G");
+                            if (shopItems[i].value > 9)
+                            {
+                                Console.WriteLine($"- {i + 1} {shopItems[i].name}| 공격력 +{shopItems[i].value} | {shopItems[i].information}| {shopItems[i].price} G");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"- {i + 1} {shopItems[i].name}| 공격력 +{shopItems[i].value}  | {shopItems[i].information}| {shopItems[i].price} G");
+                            }
                         }
 
                         else if (shopItems[i].type == 2)                //방어구
@@ -348,7 +461,14 @@ namespace Game
                     {
                         if (shopItems[i].type == 1)                 // 무기
                         {
-                            Console.WriteLine($"- {i + 1} {shopItems[i].name}| 공격력 +{shopItems[i].value}  | {shopItems[i].information}| 구매완료");
+                            if (shopItems[i].value > 9)
+                            {
+                                Console.WriteLine($"- {i + 1} {shopItems[i].name}| 공격력 +{shopItems[i].value} | {shopItems[i].information}| 구매완료");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"- {i + 1} {shopItems[i].name}| 공격력 +{shopItems[i].value}  | {shopItems[i].information}| 구매완료");
+                            }
                         }
 
                         else if (shopItems[i].type == 2)                //방어구
@@ -396,6 +516,95 @@ namespace Game
                 }
             }
 
+            void SellMyItem()
+            {
+                Console.Clear();
+                Console.WriteLine("상점 - 아이템 판매");
+                Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.\n\n[보유 골드]\n");
+                Console.WriteLine($"{character.gold} G\n\n[아이템 목록]");
+                int i = 0;
+                foreach (Item name in myItems)
+                {
+                    i++;
+                    if (name.equip)                 // 장착 했다면
+                    {
+                        if (name.type == 1)                 // 무기
+                        {
+                            if (name.value > 9)
+                            {
+                                Console.WriteLine($"- {i} [E]{name.name}| 공격력 +{name.value} | {name.information}|  {name.price} G");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"- {i} [E]{name.name}| 공격력 +{name.value}  | {name.information}|  {name.price} G");
+                            }
+                        }
+
+                        else if (name.type == 2)                //방어구
+                        {
+                            if (name.value > 9)
+                            {
+                                Console.WriteLine($"- {i} [E]{name.name}| 방어력 +{name.value} | {name.information}|  {name.price} G");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"- {i} [E]{name.name}| 방어력 +{name.value}  | {name.information}|  {name.price} G");
+                            }
+                        }
+                    }
+                    else                                    // 장착 안했다면
+                    {
+                        if (name.type == 1)                 // 무기
+                        {
+                            if (name.value > 9)
+                            {
+                                Console.WriteLine($"- {i} {name.name}   | 공격력 +{name.value} | {name.information}|  {name.price} G");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"- {i} {name.name}   | 공격력 +{name.value}  | {name.information}|  {name.price} G");
+                            }
+                        }
+
+                        else if (name.type == 2)                //방어구
+                        {
+                            if (name.value > 9)
+                            {
+                                Console.WriteLine($"- {i} {name.name}   | 방어력 +{name.value} | {name.information}|  {name.price} G");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"- {i} {name.name}   | 방어력 +{name.value}  | {name.information}|  {name.price} G");
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine("\n0. 나가기\n\n원하시는 행동을 입력해주세요.");
+                Console.Write(">>");
+
+                int sellAction = SelectNumber(myItems.Count);
+
+                if (sellAction == 0)
+                {
+                    quitSellMyItemCheck = true;
+                }
+                else
+                {
+                    int sellConfirm = 0;
+                    Console.WriteLine($"정말 {myItems[sellAction - 1].nickName}을(를) 판매하시겠습니까?\n1. 예\n0. 아니오");
+                    Console.Write(">>");
+                    sellConfirm = SelectNumber(1);
+                    if (sellConfirm == 1)
+                    {
+                        character.gold += (myItems[sellAction - 1].price * 85 / 100);
+                        myItems[sellAction - 1].equip = false;
+                        myItems[sellAction - 1].get = false;
+                        myItems.RemoveAt(sellAction - 1);
+                    }
+
+                }
+
+            }
         }
          //static public void MakeCharacter()
          //   {
@@ -679,6 +888,7 @@ namespace Game
     public class Item
     {
         public string name = "";
+        public string nickName = "";
         public int type = 0; // 1: 무기, 2: 방어구
         public int value = 0;
         public string information = "";
@@ -686,13 +896,14 @@ namespace Game
         public bool get = false;
         public bool equip = false;
 
-        public void SetItem(string Name, int Type, int Value, string Information, int Price)
+        public void SetItem(string Name, int Type, int Value, string Information, int Price, string NickName)
         {
             name = Name;
             type = Type;
             value = Value;
             information = Information;
             price = Price;
+            nickName = NickName;
         }
     }
 }
--- Game/Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
+++ Game/Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feff0baf801550fbd85b29b68c4e33f82cccd1a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c67e3e7a1f4d957180281eaf90d4bf1c15ef62ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
