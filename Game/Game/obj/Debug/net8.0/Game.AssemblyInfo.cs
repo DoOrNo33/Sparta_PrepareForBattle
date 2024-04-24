1ca508a4b652dedfb80a1e4b2b9dd855a32d6ab2
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-04-24 22:19:00 +0900

    Add_JsonSystem

--- Game/Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
+++ Game/Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55b39d7386777ca7aa1a5e346b4ed808c4f32a3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1183880340035d8533c375ef92e5550a5730aa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
