dca730f29a7a7ff54906a0a9763514296fd65af8
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-04-24 09:13:42 +0900

    Fix_Idon'tknow

--- Game/Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
+++ Game/Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a266ea77224dc3acd1179b7c150963a7cc7b2736")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41d96a13d20c45d72ff7d30785ac5d0d152bbbb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
