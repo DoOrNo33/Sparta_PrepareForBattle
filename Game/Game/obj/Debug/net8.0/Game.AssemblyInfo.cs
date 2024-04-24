5895cde747796af1920f8c9fcc9b575f4bc94f6d
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-04-24 15:38:01 +0900

    Remove_GarbageData
    필요없는 데이터 삭제

--- Game/Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
+++ Game/Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca730f29a7a7ff54906a0a9763514296fd65af8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+775937e22bbff88d4c7bbc6a04e93cb4b8606ba6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
