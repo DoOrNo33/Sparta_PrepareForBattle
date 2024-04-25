7f4e04eaf11c1c8e38f15f165adba63cdc9542e4
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-04-25 12:58:39 +0900

    stashchanged

--- Game/Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
+++ Game/Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8786f0d6a4348b4eb730d577f847cc4f9145f64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21cfa890c26e869b2b3d9511a432e74b9ad3ba32")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
